8a59f13409ecfca367777de84119455ad3f7a9b4
Eddy Ouedraogo <eddyouedraogo36@gmail.com>
2020-06-12 11:17:54 -0400

    Reproduction of the sliding item button having to be clicked twice to trigger an event

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public allItems: Array<string> = [];
 
-  constructor() {}
+    constructor(private alertCtrl: AlertController) {
+        for (let i = 0; i < 10; i++) {
+            this.allItems.push('Item ' + i);
+        }
+    }
 
+    async doSomething(item: any) {
+        const alert = await this.alertCtrl.create({
+            header: 'Alert!!!!',
+            buttons: ['OK']
+        });
+        await alert.present();
+        console.log('displaying item', item);
+    }
 }
